bfc833f050e67527a8429d553c32f07cb97c3e55
AnkitaG1401 <87742548+AnkitaG1401@users.noreply.github.com>
2022-02-26 17:31:39 +0530

    Update PrimeNumber.java
    Prime Number updated

--- src/com/guddi/PrimeNumber.java
+++ src/com/guddi/PrimeNumber.java
@@ -2,9 +2,25 @@ package com.guddi;
 
 public class PrimeNumber {
 
-	public static void main(String[] args) {
+	//
+	
+	
+	public boolean Prime(int num) {
 		
-
+		if(num==1 || num==2) {
+			return true;
+		}
+		for(int i=3;i<num/2;i++) {
+			if(num%i==0)
+				return false;			
+		}
+		return true;
+	}
+	
+	
+	public static void main(String[] args) {
+		PrimeNumber primeNumber=new PrimeNumber();
+		System.out.println(primeNumber.Prime(7));		
 	}
 
 }
